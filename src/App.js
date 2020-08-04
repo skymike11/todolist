f767220a8e54e42116de42d76e3e7300d36f3a7d
OOCLDM\lisa8 <lisa8@oocl.com>
2020-08-04 19:56:57 +0800

    [todolist][Domingo and Sam] feature : implement Menu component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import TodoList from "./component/TodoList";
 import MarkedTodoList from "./component/MarkedTodoList";
 import {HashRouter, Route} from 'react-router-dom';
+import Menu from "./component/Menu";
 
 function App() {
     return (
-        <HashRouter className="App">
-            <Route exact path = "/" component={TodoList}/>
-            <Route path = "/finish" component={MarkedTodoList}/>
-
-        </HashRouter>
-
+        <div className="App">
+            <HashRouter>
+                <Menu/>
+                <Route exact path="/" component={TodoList}/>
+                <Route path="/finish" component={MarkedTodoList}/>
+            </HashRouter>
+        </div>
     );
 }
 
