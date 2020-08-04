31af9628ea1dfb51634ae83140e42a6afea059e2
OOCLDM\lisa8 <lisa8@oocl.com>
2020-08-04 20:20:21 +0800

    [todolist][Domingo and Sam] feature : use axios to implement the Api's function

---
+++ src/api/Api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const host = "https://5e9ec500fb467500166c4658.mockapi.io/todos";
+export default {
+
+    getTodos: function () {
+        return axios.get(host + "/todos");
+    },
+    updateTodo: function (id, status) {
+        return axios.put(host + `/todos/${id}`, {"status": status})
+    },
+    addTodo: function (item) {
+        return axios.post(host + "/todos", {item})
+    },
+    deleteTodo: function (id) {
+        return axios.delete(host + `/todos/${id}`)
+    }
+
+}
